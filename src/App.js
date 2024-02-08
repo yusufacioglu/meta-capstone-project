3355b784124095a18371f4cab105c573fca19bf8
Yusuf Acıoğlu <yusuf_acioglu@aof.anadolu.edu.tr>
2024-02-09 02:43:13 +0300

    Refactor CSS and add navigation component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
+import React from 'react';
 import './App.css';
+import Nav from './components/Nav';
 
 function App() {
   return (
-    <div className="App">
-      <h1>Meta Capstone Project</h1>
-    </div>
+    <>
+      <Nav/>
+    </>
+
   );
 }
 
